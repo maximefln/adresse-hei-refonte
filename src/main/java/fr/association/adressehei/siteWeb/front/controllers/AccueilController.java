12f053c4e76aba35a1a025b4bdd96b451e5e903f
maximefln <maxime.fillion@hei.yncrea.fr>
2018-08-03 19:19:17 +0200

    feat creation de l'interface
    création des premières pages du site

--- src/main/java/fr/association/adressehei/siteWeb/front/controllers/AccueilController.java
+++ src/main/java/fr/association/adressehei/siteWeb/front/controllers/AccueilController.java
@@ -1,6 +1,7 @@
 package fr.association.adressehei.siteWeb.front.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -8,8 +9,20 @@ import org.springframework.web.servlet.ModelAndView;
 public class AccueilController {
 
     @GetMapping(value="/")
-    public ModelAndView GetHomePage(String test){
+    public ModelAndView getHomeView(){
        ModelAndView view = new ModelAndView("index.html");
+        return view;
+    }
+
+    @GetMapping(value="/adresse")
+    public ModelAndView getAdresseView(){
+        ModelAndView view =  new ModelAndView("adresse.html");
+        return view;
+    }
+
+    @GetMapping(value = "/connexion")
+    public ModelAndView getConnexionView(){
+        ModelAndView view = new ModelAndView("connexion.html");
         return view;
     }
 }
